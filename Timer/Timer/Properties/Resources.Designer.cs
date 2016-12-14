96047f0557646e77d1e8ca4506ef12e0d5f797d0
ialimpertis <ialimpertis@outlook.com>
2016-12-15 01:34:12 +0200

    Resources Removed

--- Timer/Timer/Properties/Resources.Designer.cs
+++ Timer/Timer/Properties/Resources.Designer.cs
@@ -61,25 +61,6 @@ namespace Timer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
-        /// </summary>
-        internal static System.IO.UnmanagedMemoryStream Alarm {
-            get {
-                return ResourceManager.GetStream("Alarm", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] Alarms {
-            get {
-                object obj = ResourceManager.GetObject("Alarms", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon Icon {
