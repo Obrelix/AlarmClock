77979972893759560256baed30990d65d14a852c
Obrelix <kobladoures@gmail.com>
2017-03-06 13:29:15 +0200

    Assembly Info Changes

--- Timer/Timer/Properties/AssemblyInfo.cs
+++ Timer/Timer/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alarm Timer")]
-[assembly: AssemblyDescription("This is an application for multiple allarm clocks with custom video allarm or music allarm")]
+[assembly: AssemblyTitle("Alarm Clock")]
+[assembly: AssemblyDescription("Customable Allarm Clocks")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Obrelix")]
 [assembly: AssemblyProduct("Allarm Clock")]
--- Timer/Timer/Properties/Resources.Designer.cs
+++ Timer/Timer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Timer.Properties {
+namespace AlarmClock.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Timer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Timer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AlarmClock.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
